fc3b7a0762d87b7f0d0887d476d7a92d78b3ec68
FogAndCloud <2214199594@qq.com>
2024-05-01 16:42:05 +0800

    添加技能使用按钮

--- Assets/Scripts/PanelCtrl/BKPanelCtrl.cs
+++ Assets/Scripts/PanelCtrl/BKPanelCtrl.cs
@@ -8,10 +8,36 @@ using UnityEngine.UI;
 
 public class BKPanelCtrl : BasePanel
 {
+    // 静态实例变量  
+    private static BKPanelCtrl _instance;
+    // 私有构造函数，防止外部直接实例化  
+    private BKPanelCtrl() { }
+    // 静态属性，用于获取单例实例  
+    public static BKPanelCtrl Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+    //唤醒时获取单例实例
+    private void OnEnable()
+    {
+        _instance = this;
+    }
+
     private Image startAdvPoint;
     private Image freeBatPoint;
     private Image batPoint;
-    private Button endRound;
+    private Button endRound_btn;
+    private Button left_btn;
+    private Button right_btn;
+
+    public string left_text;
+    public string right_text;
+    public UnityAction leftAction;
+    public UnityAction rightAction;
+    
     void Start()
     { 
         Init();
@@ -22,15 +48,22 @@ public class BKPanelCtrl : BasePanel
         startAdvPoint = GetControl<Image>("StartAdvPoint");
         freeBatPoint = GetControl<Image>("FreeBatPoint");
         batPoint= GetControl<Image>("BatPoint");
-        endRound = GetControl<Button>("EndRound");
-        endRound.gameObject.SetActive(false);
-        
+        endRound_btn = GetControl<Button>("EndRound");
+        left_btn = GetControl<Button>("Left_btn");
+        right_btn = GetControl<Button>("Right_btn");
+        left_text = "使用技能";
+        right_text = "取消";
+        left_btn.GetComponentInChildren<TextMeshProUGUI>().text = left_text;
+        right_btn.GetComponentInChildren<TextMeshProUGUI>().text = right_text;
+        endRound_btn.gameObject.SetActive(false);
+        right_btn.gameObject.SetActive(false);
+        left_btn.gameObject.SetActive(false);
         EventCenter.GetInstance().addEventListener<AdvCardView>("StartAdv", (obj) =>
         {
             //固定使用的冒险卡位置
             BaseCard.GetInstance().TurnOverCard(obj, obj.IsEnable, startAdvPoint.transform);
             //显示回合结束按钮
-            endRound.gameObject.SetActive(true);
+            endRound_btn.gameObject.SetActive(true);
         });
         UIManager.GetInstance().ShowPanel<FreeBatPanelCtrl>("Panel/FreeBatPanel", E_UI_Layer.Mid, (panel) =>
         {
@@ -63,11 +96,14 @@ public class BKPanelCtrl : BasePanel
             case "EndRound":
                 //结算回合 计算血量 是否销毁卡牌 是否获得战斗卡
                 EventCenter.GetInstance().EventTrigger("EndRoundOfHp");
-                endRound.gameObject.SetActive(false);
-
+                endRound_btn.gameObject.SetActive(false);
+                 
                 break;
-            case "AdvDiscardName_btn":
-
+            case "Left_btn":
+                leftAction();
+                break;
+            case "Right_btn":
+                rightAction();
                 break;
         }
     }
